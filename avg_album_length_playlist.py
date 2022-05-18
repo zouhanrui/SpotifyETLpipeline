d57e5695d71c37f1137a41cd2c3e597f00d7ec9a
Hanrui Zou <zouhanrui@gmail.com>
2022-05-18 01:46:48 -0700

    first commit for prj SpotifyETL

---
+++ avg_album_length_playlist.py
@@ -0,0 +1,9 @@
+import spotipy
+import csv
+import boto3
+from datetime import datetime
+
+from config.playlists import spotify_playlists
+
+print('hello')
+
---
+++ config/playlists.py
@@ -0,0 +1,8 @@
+def spotify_playlists():
+    playlists = {'Eminem - Recovery': 'spotify:playlist:5tBepYE6DAMSQgLC2MvWam'}
+    return playlists
+
+
+def personal_playlists():
+    playlists = {'Eminem - Recovery': 'spotify:playlist:5tBepYE6DAMSQgLC2MvWam'}
+    return playlists
