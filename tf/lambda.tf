4c140455bb170cbe0f9ebe9006a7bc096926f098
Hanrui Zou <zouhanrui@gmail.com>
2022-06-11 12:47:10 -0700

    modified the path of payload

--- tf/lambda.tf
+++ tf/lambda.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "spotify_analysis" {
-  filename = "../payload.zip"
+  filename = "../../payload.zip"
   function_name = "spotify_analysis"
   handler = "avg_album_length_playlist.lambda_handler"
   role = "${aws_iam_role.lambda_execution_role.arn}"
